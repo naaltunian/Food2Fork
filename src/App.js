9eab7970c425dc903638b91717138d318e109f2a
naaltunian <naaltunian@icloud.com>
2019-03-05 12:59:48 -0500

    Add api call and set state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ import RecipeDetails from './Components/RecipeDetails';
 import apiKey from './config';
 
 class App extends Component {
+
+  state = {
+    recipes: [],
+    url: `https://www.food2fork.com/api/search?key=${apiKey}`
+  }
+
+  getRecipes = async () => {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({ recipes: jsonData.recipes });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
   render() {
+    console.log(this.state.recipes);
     return (
       <div>
         <RecipeList />
