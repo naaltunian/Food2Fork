92334a17d0777cdd84d0433756eb9c1b757607c0
naaltunian <naaltunian@icloud.com>
2019-03-05 09:53:49 -0500

    add skeleton frames for all Recipe Components

---
+++ src/Components/RecipeDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RecipeDetails extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                RecipeDetails Comp
+            </React.Fragment>
+        )
+    }
+}
